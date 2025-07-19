a6bdb3555673fae2658d5cadd0cfcafed1f343de
Ghost-web-ops <omaryasser011549@gmail.com>
2025-07-19 08:18:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(passport.initialize());
-app.use(cors());
+app.use(cors(
+{   origin: "https://task-manager-frontend-alna.vercel.app", // ⚠️ Replace with your Vercel URL
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],}
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', userRoutes);
